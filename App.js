9be5be618aaf5ec32a963c3ddf8d0b766d414163
khaledtasmia <khaled.tasmia33@gmail.com>
2021-06-03 22:08:29 +0600

    task updated

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hello World!</Text>
+      <Text>Team 97</Text>
       <StatusBar style="auto" />
     </View>
   );
