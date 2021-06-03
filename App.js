9e9f0d1bda9dffb81b5235587f3616b45f5a4c03
khaledtasmia <khaled.tasmia33@gmail.com>
2021-06-04 01:39:58 +0600

    task component completed

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './component/Task/Task';
 
 export default function App() {
+
+  const [task, setTask] = useState();
+
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World!</Text>
-      <Text>Team 97</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskPart}>
+        <Text style={styles.title}>Today's Tasks</Text>
+        <View style={styles.taskList}>
+          {
+            taskItems.map( eachTask => <Task text={eachTask}></Task>)
+          }
+        </View>
+      </View>
+
+    <KeyboardAvoidingView
+    behavior={Platform.OS === "android" ? "padding" : "height"}
+    style={styles.writeATaskBox}>
+      <TextInput style={styles.input} placeholder={'Write A Task'} onChangeText={text => setTask(text)} value={task}></TextInput>
+      <TouchableOpacity onPress={ () => handleAddTask()}>
+        <View style={styles.addBtn}>
+          <Text style={styles.addTask}>+</Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
     </View>
   );
 }
@@ -16,8 +42,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#7ea6e6',
   },
+  taskPart: {
+    paddingTop: 80,
+    paddingHorizontal: 20
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  taskList: {
+    marginTop: 30
+  },
+  writeATaskBox: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    width: 250,
+    paddingHorizontal: 15,
+    backgroundColor: 'white',
+    borderRadius: 60,
+    width: 250
+  },
+  addBtn: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addTask: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 3
+  }
 });
--- component/Task/Task.js
+++ component/Task/Task.js
@@ -1,12 +1,50 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
-export default function Task() {
+export default function Task(props) {
     return (
-        <View>
-            <Text>This is for task list.</Text>
+        <View style={styles.taskBox}>
+            <View style={styles.taskLeft}>
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <Text style={styles.taskField}>{props.text}</Text>
+            </View>
+            <View>
+                <Text style={styles.cross}>x</Text>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    taskBox: {
+        backgroundColor: 'white',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+    taskLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#0932e6',
+        borderRadius: 5,
+        marginRight: 15
+    },
+    taskField: {
+        maxWidth: '80%',
+        fontSize: 15
+    },
+    cross: {
+        fontSize: 20,
+        marginBottom: 5,
+        fontWeight: 'bold',
+        color: '#0932e6'
+    }
+});
