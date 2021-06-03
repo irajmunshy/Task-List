c90c3cfc8ce90642b96d7872dbe59d25222f2eb1
khaledtasmia <khaled.tasmia33@gmail.com>
2021-06-03 22:12:04 +0600

    task component updated

---
+++ component/Task/Task.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Task() {
+    return (
+        <View>
+            <Text>This is for task list.</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
